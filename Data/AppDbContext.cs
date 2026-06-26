70a4b427c10a6b58a49e6544bf204433775aa90e
David Marchiori <davidmarchiorint@gmail.com>
2026-06-25 22:48:28 -0300

    First commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WalletView.Models;
+
+namespace WalletView.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
